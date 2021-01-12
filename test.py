e3bacf307a113c95891c60763188362a569ca17c
munnytravels <70662658+munnytravels@users.noreply.github.com>
2021-01-12 16:48:36 +0000

    made changes and will now try and sync to local

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
 print("Hello world")
-print("This is a test, please work for the love of goodness.")
 def capitalise_first_word(sentence):
     sentence = sentence.split()
     sentence[0] = sentence[0].upper()
     sentence = " ".join(sentence)
     return (sentence)
-
-print(capitalise_first_word("hello everybody good people"))
 
